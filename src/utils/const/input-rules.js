e16c590664fef639a88543753721779a0c1202d6
Sebastian <sebastian.urquijo@enviame.io>
2023-12-21 12:00:08 -0500

    set fiel name required by prisma model

--- src/utils/const/input-rules.js
+++ src/utils/const/input-rules.js
@@ -1,5 +1,5 @@
 const rules = {
-    nombre: 'string|min:3|max:15',
+    nombre: 'string|min:3|max:15|required',
     apellido: 'string|min:3|max:20',
     edad: 'integer',
     celular: 'string|max:10|required',
