abc308aebacfef0195eaa7e716566b5433741c19
omarerodriguez <omarmr7214@gmail.com>
2023-12-26 19:09:02 +0400

    usecases createNewNailsTypes

--- src/application/usecases/nailsTypes-usecases.js
+++ src/application/usecases/nailsTypes-usecases.js
@@ -1,20 +1,34 @@
-
+const { getFormatDate } = require('../../utils/functions/date');
 module.exports = class NailsTypesUseCases {
-    constructor(prismaRepository){
-        this.prismaRepository = prismaRepository;
-    }
-    
-    findNailsTpById = async(nailsTpId){
-        const [findNailsTp,err] = await this.prismaRepository.findNailsTpById(nailsTpId);
-        if(err)return [null,404,err];
-        return [findNailsTp,200,null];
-    };
+  constructor(prismaRepository) {
+    this.prismaRepository = prismaRepository;
+  }
 
-    findAllNailsTypes = async(){
-        const [nailsTypes,err] = await this.prismaRepository.findAllNailsTypes();
-        if(err)return [null,404,err];
-        return [nailsTypes,200,null];
-    };
+  findNailsTpById = async (nailsTpId) => {
+    const [findNailsTp, err] =
+      await this.prismaRepository.findNailsTpById(nailsTpId);
+    if (err) return [null, 404, err];
+    return [findNailsTp, 200, null];
+  };
 
-    
+  findAllNailsTypes = async () => {
+    const [nailsTypes, err] = await this.prismaRepository.findAllNailsTypes();
+    if (err) return [null, 404, err];
+    return [nailsTypes, 200, null];
+  };
+
+  createNewNailsTypes = async (nailsTypesPayload) => {
+    const [findNailsTp, nailsError] =
+      await this.prismaRepository.findNailsTpById(nailsTypesPayload.id);
+    if (nailsError) return [null, 500, nailsError];
+    if (findNailsTp) return [null, 400, 'NailsTypes already exist'];
+
+    const newNailsTypeBody = { ...nailsTypesPayload };
+    newNailsTypeBody.created_at = getFormatDate();
+
+    const [newNailsType, err] =
+      await this.prismaRepository.createNewNailsTypes(newNailsTypeBody);
+    if (err) return [null, 400, err];
+    return [newNailsType, 201, null];
+  };
 };
