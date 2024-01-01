faebd433720ba7d8ce9776e749c27f6acc089841
omarerodriguez <omarmr7214@gmail.com>
2024-01-01 20:24:20 +0400

    findScheduler-repository

--- src/adapters/repositories/scheduler-prisma-repository.js
+++ src/adapters/repositories/scheduler-prisma-repository.js
@@ -15,4 +15,17 @@ module.exports = class SchedulerPrismaRepository {
       );
     }
   }
+  async findSchedulerById(schedulerId) {
+    try {
+      const scheduler = await this.prismaClient.scheduler.findFirst({
+        where: { id: schedulerId },
+      });
+      if (!scheduler) return [null, `Scheduler not found`];
+      return [scheduler, null];
+    } catch (error) {
+      throw new Error(
+        `there was a error in scheduler-prisma-repository.findSchedulerById err: ${error.message}`,
+      );
+    }
+  }
 };
