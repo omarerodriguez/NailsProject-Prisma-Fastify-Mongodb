a957498f69a046df97824cfef850673aeda259c5
Sebastian <sebastian.urquijo@enviame.io>
2024-01-05 13:32:56 -0500

    add validations of exist escheduler and create updated shcdeler of respective service in it usecase

--- src/application/usecases/scheduler-usecases.js
+++ src/application/usecases/scheduler-usecases.js
@@ -1,5 +1,5 @@
 const { getFormatDate } = require('../../utils/functions/date');
-
+const { setAppoinments, updateAppoinments } = require('../../utils/functions/scheduler');
 module.exports = class SchedulerUseCases {
   constructor(prismaRepository) {
     this.prismaRepository = prismaRepository;
@@ -18,21 +18,29 @@ module.exports = class SchedulerUseCases {
     return [scheduler, 200, null];
   };
 
-  createNewScheduler = async (schedulerPayload) => {
-    const newScheduler = { ...schedulerPayload };
+  createNewScheduler = async (schedulerPayload = {}) => {
+    const newScheduler = setAppoinments(schedulerPayload);
     newScheduler.created_at = getFormatDate();
 
     const [scheduler, err] = await this.prismaRepository.createNewScheduler(
-      schedulerPayload,
+      newScheduler,
     );
     if (err) return [null, 404, err];
     return [scheduler, 200, null];
   };
 
-  updateScheduler = async (schedulerId, schedulerPayload) => {
+  updateScheduler = async (schedulerId, appointmentId, userId) => {
+    const [scheduler, errScheduler] = await this.prismaRepository.findSchedulerById(
+      schedulerId,
+    );
+    if (errScheduler) return [null, 404, err];
+
+    const [updatedScheduler, errUpdateAppoinment] = updateAppoinments(scheduler,"APOINMENT", "appointment.reserved_at", "appointment.duration");
+    if (errUpdateAppoinment) return [null, 400, errUpdateAppoinment];
+
     const [updateScheduler, err] = await this.prismaRepository.updateScheduler(
       schedulerId,
-      schedulerPayload,
+      updatedScheduler,
     );
     if (err) return [null, 404, err];
     return [updateScheduler, 200, null];
