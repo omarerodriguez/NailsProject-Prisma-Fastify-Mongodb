d746a7e55a1e6f699d79e16e86c7ab5472588b07
omarerodriguez <omarmr7214@gmail.com>
2024-01-06 11:11:46 +0400

    findAllAppointmentById-repository

--- src/adapters/repositories/appointment-prisma-repository.js
+++ src/adapters/repositories/appointment-prisma-repository.js
@@ -15,4 +15,17 @@ module.exports = class AppointmentPrismaRepository {
       );
     }
   }
+  async findAppointmentById(appointmentId) {
+    try {
+      const appointment = await this.prismaClient.appointment.findFirst({
+        where: { id: appointmentId },
+      });
+      if (!appointment) return [null, `Appointment not found`];
+      return [appointment, null];
+    } catch (error) {
+      throw new Error(
+        `there was a error in appointment-prisma-repository.findAppointmentById err: ${error.message}`,
+      );
+    }
+  }
 };
