6a9d2d8f5fd2964624c749387f1568f5a7f4caaf
omarerodriguez <omarmr7214@gmail.com>
2024-01-25 15:27:22 +0400

    some fields changed

--- src/utils/const/custom-messages.js
+++ src/utils/const/custom-messages.js
@@ -1,18 +1,16 @@
 const customMessagesCreateUser = {
   'required.name': 'El campo :attribute(nombre) es obligatorio.',
   'string.name': 'El campo :attribute(nombre) deben ser letras',
-  'min.name': 'El campo :attribute(nombre) tener mínimo 3',
-  'max.name': 'El campo :attribute(nombre) debe tener máximo 15 caracteres',
-  'string.last_name':
-    'El campo:attribute deben ser letras y debe tener mínimo 3 y máximo 20 caracteres',
-  'min.last_name': 'El campo :attribute(apellido) tener mínimo 3',
-  'max.last_name':
-    'El campo :attribute(apellido) debe tener máximo 20 caracteres',
+  'between.name':
+    'El campo :attribute(nombre) debe tener mínimo 3 y máximo 15 caracteres',
+  'string.last_name': 'El campo :attribute deben ser letras',
+  'between.last_name':
+    'El campo :attribute(apellido) debe tener mínimo 3 y máximo 20 caracteres',
   'integer.age': 'El campo :attributr(edad) deber ser un número',
   'required.phone_number': 'El campo :attribute(celular) es obligatorio.',
   'string.phone_number':
     'El campo :attribute(celular) el numero debe ir entre comillas dobles',
-  'max.phone_number':
+  'size.phone_number':
     'El campo :attribute(celular) debe ser de máximo de 10 dígitos',
   'string.district': 'El campo :attribute(barrio) deben ser letras',
   'max.district':
@@ -27,30 +25,27 @@ const customMessagesCreateUser = {
 const customMessagesCreateNailsTypes = {
   'required.name': 'El campo :attribute es obligatorio.',
   'string.name': 'El campo :attribute(nombre) deben ser letras.',
-  'min.name': 'El campo :attribute(nombre) tener mínimo 3',
-  'max.name': 'El campo :attribute(nombre) debe tener máximo 25 caracteres',
+  'between.name':
+    'El campo :attribute(nombre) tener mínimo 4 y máximo 25 caracteres',
   'integer.default_price':
     'El campo :attribute(precio por defecto) debe ser un número',
-  'min.default_price':
-    'El campo :attribute(precio por defecto) el monto mínimo debe ser de $500 (COP)',
-  'max.default_price':
-    'El campo :attribute(precio por defecto) el monto máximo debe ser de $1M (COP)',
+  'between.default_price':
+    'El campo :attribute(precio por defecto) el monto mínimo debe ser de $500 y máximo debe ser de $1M (COP)',
 };
 
 const customMessagesCreateDetailsNails = {
   'required.name': 'El campo :attribute(nombre) es obligatorio.',
   'string.name': 'El campo :attribute(nombre) deben ser letras',
-  'min.name': 'El campo :attribute(nombre) tener mínimo 3',
-  'max.name': 'El campo :attribute(nombre) debe tener máximo 25 caracteres',
+  'between.name':
+    'El campo :attribute(nombre) tener mínimo 3 y  máximo 25 caracteres',
   'integer.price': 'El campo :attribute(precio) debe ser un numero',
-  'min.price':
-    'El campo :attribute(precio) el monto mínimo debe ser de $500 (COP)',
-  'max.price': 'El campo :attribute(precio) el monto máximo es de $1m (COP)',
+  'between.price':
+    'El campo :attribute(precio) el monto mínimo debe ser de $500 y máximo es de $1M (COP)',
 };
 
 const customMessagesLoginUser = {
   'required.phone_number': 'El campo :attribute(celular) es obligatorio.',
-  'max.phone_number':
+  'size.phone_number':
     'El campo :attribute(celular) debe ser de máximo de 10 dígitos',
   'required.email': 'El campo :attribute(correo) es obligatorio',
   'email.email':
