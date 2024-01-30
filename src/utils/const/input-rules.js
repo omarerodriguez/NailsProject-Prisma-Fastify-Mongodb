afcce92c7d0b5ec61367d9d3f47802484ae1d7ef
omarerodriguez <omarmr7214@gmail.com>
2024-01-30 12:09:54 +0400

    field changed - createNewAppointment

--- src/utils/const/input-rules.js
+++ src/utils/const/input-rules.js
@@ -37,7 +37,7 @@ const getUserByIdRules = {
 
 const createNewAppointmentRules = {
   user_id: 'string|hex|size:24|required',
-  types_of_nails_id: 'string|array|hex|size:24|required',
+  types_of_nails_id: 'string|hex|size:24|required',
   details_of_nails: 'string|array|hex|size:24|required',
   duration: 'integer|max:10',
   reserved_at: 'string',
