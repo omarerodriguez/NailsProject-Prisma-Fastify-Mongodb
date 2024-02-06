45f14c2c3cc64e8add5bab40bc67c5942471968e
omarerodriguez <omarmr7214@gmail.com>
2024-02-06 20:22:48 +0400

    types-nails verifyRoleToken

--- src/adapters/http/nails/nails-types-route.js
+++ src/adapters/http/nails/nails-types-route.js
@@ -1,29 +1,37 @@
-const { nailsTypesHandler } = require('../../../utils/intances-usecases');
+const {
+  nailsTypesHandler,
+  tokenMiddleWare,
+} = require('../../../utils/intances-usecases');
 
 const routes = [
   {
     url: '/nailstypes',
     method: 'GET',
+    preHandler: [tokenMiddleWare.verifyUserToken],
     handler: nailsTypesHandler.findAllNailsTypes,
   },
   {
     url: '/nailstypes/:id',
     method: 'GET',
+    preHandler: [tokenMiddleWare.verifyUserToken],
     handler: nailsTypesHandler.findNailsTypesById,
   },
   {
     url: '/nailstypes',
     method: 'POST',
+    preHandler: [tokenMiddleWare.verifyAdminToken],
     handler: nailsTypesHandler.createNewNailsTypes,
   },
   {
     url: '/nailstypes/:id',
     method: 'PUT',
+    preHandler: [tokenMiddleWare.verifyAdminToken],
     handler: nailsTypesHandler.updateNailsTypes,
   },
   {
     url: '/nailstypes/:id',
     method: 'DELETE',
+    preHandler: [tokenMiddleWare.verifyAdminToken],
     handler: nailsTypesHandler.deleteNailsTypes,
   },
 ];
