a1c40de902e7e285262b603888166d4980444bf6
omarerodriguez <omarmr7214@gmail.com>
2024-02-16 15:17:28 +0400

    generateToken function test

--- src/utils/functions/token-funtion.js
+++
@@ -1,6 +0,0 @@
-const extractTokenToHeader = (reqObject) =>
-  reqObject.headers?.authorization?.split(' ')[1];
-
-module.exports = {
-  extractTokenToHeader,
-};
--- test/application/token-usecases/token-usecases.test.js
+++ test/application/token-usecases/token-usecases.test.js
@@ -2,22 +2,29 @@ const jwt = require('jsonwebtoken');
 /** TokenUseCases */
 const TokenUseCases = require('../../../src/application/usecases/token-usecases');
 
-/**Intances UseCase */
-const tokenUseCases = new TokenUseCases(jwt);
-
 describe('generateToken function', () => {
-  // Mockear el objeto `jwt` para evitar llamadas reales a `jwt.sign`
-  jest.spyOn(jwt, 'sign').mockResolvedValue('fake_token');
-  //jest.fn(process.env.JWT_SECRET_KEY).mockResolvedValue('fake_secret_key');
-  jest.spyOn(jwt, 'verify').mockImplementation((token, secret) => {
-    if (token === 'valid_token') {
-      return { userId: '659936dc6a1d92adb561073ex', role: 'ADMIN' };
-    } else if (token === 'expired_token') {
-      throw new jwt.TokenExpiredError('Token has expired');
-    } else {
-      throw new Error('Invalid token');
-    }
+  let tokenUseCases;
+  beforeAll(() => {
+    /**Intances UseCase */
+    tokenUseCases = new TokenUseCases(jwt);
   });
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.spyOn(jwt, 'sign').mockResolvedValue('fake_token');
+    //jest.fn(process.env.JWT_SECRET_KEY).mockResolvedValue('fake_secret_key');
+    jest.spyOn(jwt, 'verify').mockImplementation((token, secret) => {
+      if (token === 'valid_admin_token') {
+        return { userId: '659936dc6a1d92adb561073ex', role: 'ADMIN' };
+      } else if (token === 'user_token') {
+        return { userId: '659936dc6a1d92adb561072ex', role: 'USER' };
+      } else if (token === 'expired_token') {
+        throw new jwt.TokenExpiredError('Token has expired');
+      } else {
+        throw new Error('Invalid token');
+      }
+    });
+  });
+
   /**GENERATE TOKEN TEST */
   test('should generate a valid token', async () => {
     process.env.JWT_SECRET_KEY = 'fake_secret_key';
@@ -36,7 +43,7 @@ describe('generateToken function', () => {
       },
     );
   });
-  /*
+
   test('should return an error when userId is empty', async () => {
     const [token, error] = await tokenUseCases.generateToken(null, 'admin');
 
@@ -46,10 +53,10 @@ describe('generateToken function', () => {
   });
 
   /**VERIFY ROLE TEST */
-  /*
+
   test('should verify a valid token with correct role', async () => {
-    const token = 'valid_token';
-    const role = 'ADMIN';
+    const token = 'valid_admin_token';
+    const role = ['ADMIN'];
     const [decodedToken, status, error] = await tokenUseCases.verifyToken(
       token,
       role,
@@ -60,6 +67,19 @@ describe('generateToken function', () => {
       role: 'ADMIN',
     });
     expect(status).toBe(200);
-    expect(error).toBe('Invalid token')
-  }); */
+    expect(error).toBeNull();
+  });
+
+  test('should verify a valid token with incorrect role', async () => {
+    const token = 'user_token';
+    const role = ['ADMIN'];
+    const [decodedToken, status, error] = await tokenUseCases.verifyToken(
+      token,
+      role,
+    );
+
+    expect(decodedToken).toBeNull();
+    expect(status).toBe(403);
+    expect(error).toBe('Unauthorized');
+  });
 });
