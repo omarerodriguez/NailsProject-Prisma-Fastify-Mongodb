6f601790c340cd0239f7f6f393bff0aa55d7547a
omarerodriguez <omarmr7214@gmail.com>
2024-02-26 15:34:58 +0400

    regex updated

--- src/utils/const/custom-rules.js
+++ src/utils/const/custom-rules.js
@@ -1,7 +1,9 @@
 const Validator = require('validatorjs');
 
 const customPriceRule = (value) => {
-  const regex = /^[1-9]\d{0,2}(?:\.\d{3})*(?:,\d{1,2})?$/;
+  const regex =
+    /^(?:[5-9]\d{0,2}(?:\.\d{3})*(?:,\d{1,2})?|[1-9]\d{3,}(?:\.\d{3})*(?:,\d{1,2})?)$/;
+
   return regex.test(value);
 };
 Validator.register('customPrice', customPriceRule);
