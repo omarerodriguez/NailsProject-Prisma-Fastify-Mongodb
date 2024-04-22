a894d4e4b270bf6f4665e20d1ea558158293954c
Omar Enrique Rodríguez Mantilla <omarmr7214@gmail.com>
2024-04-21 21:15:53 -0500

    buildRecordTypesNails registed in index

--- src/application/usecases/builder/appointment/index.js
+++ src/application/usecases/builder/appointment/index.js
@@ -1,6 +1,9 @@
 const {
   buildRecordAppointment,
 } = require('../../builder/appointment/find-all-appointment-builder');
+
+const {buildRecordTypesNails} = require('../../builder/nails/find-all-details-builder');
 module.exports = {
   buildRecordAppointment,
+  buildRecordTypesNails
 };
