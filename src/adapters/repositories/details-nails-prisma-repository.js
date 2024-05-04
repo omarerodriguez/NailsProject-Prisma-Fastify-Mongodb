afd5387100dd567511ec1c9d763efc280783eeda
Omar Enrique Rodríguez Mantilla <omarmr7214@gmail.com>
2024-05-04 09:26:07 -0500

    redisClient into detailsNailsRepository added

--- src/adapters/repositories/details-nails-prisma-repository.js
+++ src/adapters/repositories/details-nails-prisma-repository.js
@@ -1,6 +1,7 @@
 module.exports = class DetailsNailsPrismaRepository {
-  constructor(prismaClient) {
+  constructor(prismaClient,redisClient) {
     this.prismaClient = prismaClient;
+    this.redisClient = redisClient;
   }
   async findAllDetailsNails(ids) {
     try {
